6655dbbddb1ec02237bd9a447490489b6e68105b
Vitaly Mozhaev <69746684+VitalyMozhaev@users.noreply.github.com>
2022-09-22 13:12:40 +0300

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,4 +5,16 @@ terraform {
       version = "0.75.0"
     }
   }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "dp-bucket"
+    region     = "ru-central1-a"
+    key        = "terraform.tfstate.d/stage/terraform.tfstate"
+    access_key = "YC...oJ"
+    secret_key = "YCO...R8F"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
