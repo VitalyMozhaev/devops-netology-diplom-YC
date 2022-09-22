800b9a6be6edc8791a9f82d7f1a1daed1310c1a6
Vitaly Mozhaev <69746684+VitalyMozhaev@users.noreply.github.com>
2022-09-22 13:11:26 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,3 +23,11 @@ output "node2-internal-ip" {
 output "node2-external-ip" {
   value = yandex_compute_instance.node2.network_interface.0.nat_ip_address
 }
+output "access_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  sensitive = true
+}
+output "secret_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  sensitive = true
+}
